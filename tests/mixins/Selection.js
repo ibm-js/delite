a4d9e361ab1ea754dc212d22e51a5de91db813a9
Christophe Jolif <cjolif@gmail.com>
2013-02-28 14:04:47 +0100

    fix & improve test

--- tests/mixins/Selection.js
+++ tests/mixins/Selection.js
@@ -1,7 +1,7 @@
 define(["doh", "dojo/_base/declare", "../../mixins/Selection", "../../_WidgetBase"],
 	function(doh, declare, Selection, _WidgetBase){
 	doh.register("mixins.Selection", [
-		function test_Lifecycle(t){
+		function test_SetGet(t){
 			var C = declare("MyWidget", [_WidgetBase, Selection], {
 				updateRenderers: function(){
 				}
@@ -16,6 +16,23 @@ define(["doh", "dojo/_base/declare", "../../mixins/Selection", "../../_WidgetBas
 			o = new C({selectedItem: "1"});
 			t.is("1", o.get("selectedItem"));
 			t.is(["1"], o.get("selectedItems"));
+		},
+		function test_Event(t){
+			var d = new doh.Deferred();
+			var C = declare("MyWidget", [_WidgetBase, Selection], {
+				updateRenderers: function(){
+				},
+				getIdentity: function(item){
+					return item;
+				}
+			});
+			var o = new C({selectedItem : "1"});
+			o.on("change", d.getTestCallback(function(evt){
+				t.is("1", evt.oldValue);
+				t.is("2", evt.newValue);
+			}));
+			o.startup();
+			o.selectFromEvent({}, "2", null, true);
 		}
 	]);
 });
