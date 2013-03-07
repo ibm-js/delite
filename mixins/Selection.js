cf80c4d2ff2f4f66b28b9f18c7777eb0e0c24366
Christophe Jolif <cjolif@gmail.com>
2013-03-07 15:28:12 +0100

    remove some bases classes that anway comes from the receiving class. Update doc to mention that.

--- mixins/Selection.js
+++ mixins/Selection.js
@@ -1,6 +1,5 @@
 define(["dojo/_base/declare", "dojo/_base/array", "dojo/_base/lang"],
 	function(declare, arr, lang){
-
 	return declare(null, {
 		// summary:
 		//		Mixin for classes for widgets that manage a list of selected data items. Receiving class must extend
