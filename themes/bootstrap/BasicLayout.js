86428d0e2794b5b6f4de8b30adc7900d091ed229
Damien Mandrioli <dmandrioli@gmail.com>
2013-11-19 15:20:55 +0100

    fix vertical layout for FF

--- themes/bootstrap/BasicLayout.js
+++ themes/bootstrap/BasicLayout.js
@@ -17,6 +17,7 @@ define(function(){ return '\
   -webkit-box-orient: vertical;\
   -moz-box-orient: vertical;\
   -ms-box-orient: vertical;\
+  flex-direction: column;\
 }\
 .fill {\
   -webkit-box-flex: 1;\
