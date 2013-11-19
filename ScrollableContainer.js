35f19c7b9e521bc05a5f7aad7e6925a928117266
Adrian Vasiliu <vasiliu@fr.ibm.com>
2013-11-19 18:31:15 +0100

    Programmatic scroll now supports both horiz and vert scroll; various other code and doc fixes; code formatting; added programmatic scroll to the test

--- ScrollableContainer.js
+++ ScrollableContainer.js
@@ -4,12 +4,11 @@ define([
 	"dojo/dom-style",
 	"dojo/_base/fx",
 	"dojo/fx/easing",
-	// "./mobile/Container",
 	"./register",
 	"./Widget",
 	"./Container",
 	"./themes/load!ScrollableContainer"
-], function(dcl, dom, domStyle, baseFx, easing, register, Widget, Container){
+], function (dcl, dom, domStyle, baseFx, easing, register, Widget, Container) {
 
 	// module:
 	//		dui/ScrollableContainer
@@ -18,70 +17,105 @@ define([
 		// summary:
 		//		A container widget with scrolling capabilities.
 		// description:
-		//		Container is a container widget which can scroll its content.
+		//		A container widget which can scroll its content 
+		//		horizontally and/or vertically.
 
-		// scrollDir: String
-		//		v: vertical, h: horizontal, vh: both, f: flip
-		scrollDir: "v",
+		// scrollDirection: String
+		//		The direction of the interactive scroll:
+		//		v: vertical, h: horizontal, vh: both.
+		//		Note that scrolling programmatically using scrollTo() is
+		//		possible on both direction independently on the value of scrollDirection.
+		scrollDirection: "v",
 		
 		// baseClass: String
 		//		The name of the CSS class of this widget.
 		baseClass: "duiScrollableContainer",
 		
-		buildRendering: function(){
+		buildRendering: function () {
 			this.containerNode = this;
 			dom.setSelectable(this.containerNode, false);
 			
-			if(this.scrollDir.indexOf("v") != -1){ 
-				domStyle.set(this.containerNode, "overflowY", "scroll"); 
-			} 
-			if(this.scrollDir.indexOf("h") != -1 || 
-				this.scrollDir.indexOf("f") != -1){ 
-				domStyle.set(this.containerNode, "overflowX", "scroll"); 
-			} 
+			if (this.scrollDirection.indexOf("v") !== -1) {
+				domStyle.set(this.containerNode, "overflowY", "scroll");
+			}
+			if (this.scrollDirection.indexOf("h") !== -1) {
+				domStyle.set(this.containerNode, "overflowX", "scroll");
+			}
 		},
 		
-		scrollTo: function(y, /*Number?*/duration){
+		// TODO: custom setter for scrollDirection? The difficult point is what
+		// value to restore for overflowX/Y when the user changes the scrollDirection
+		// afterwards. The default one? Or should we store the initial value such that
+		// we can restore it? Needs more thinking.
+		
+		getCurrentScroll: function () {
+			// summary:
+			//		Returns the current amount of scroll, as an object with x and y properties
+			//		for the horizontal and vertical scroll amount. TODO: improve the doc.
+			return {x: this.containerNode.scrollLeft, y: this.containerNode.scrollTop};
+		},
+		
+		scrollTo: function (to, /*Number?*/duration) {
 			// summary:
 			//		Scrolls to the given position.
 			// to:
-			//		The scroll destination position. An object with x and/or y.
-			//		ex. {x:0, y:-5}, {y:-29}, etc.
+			//		The scroll destination position. An object with x and/or y, for example
+			//		{x:0, y:-5} or {y:-29}.
 			// duration:
-			//		Duration of scrolling in milliseconds.
+			//		Duration of scrolling animation in milliseconds. If 0 or unspecified,
+			//		scrolls without animation. 
 			
-			// TODO: depending on whether we will want to support horizontal scroll
-			// in addition to vertical scroll, we will transform the "to" argument into 
-			// an object with x and/or y properties.
 			var self = this;
 			var domNode = this.containerNode;
-			if(duration <= 0){ // shortcut
-				domNode.scrollTop = y;
-				return;
-			}
-			var animation = function(y){
-				if(self._animation && self._animation.status() == "playing"){
-					self._animation.stop();
+			if (!duration || duration <= 0) { // shortcut
+				if (to.x) {
+					domNode.scrollLeft = to.x;
 				}
-				var	anim = new baseFx.Animation({
-					beforeBegin: function(){
-						if(this.curve){
-							delete this.curve;
-						}
-						anim.curve = new baseFx._Line(domNode.scrollTop, y);
-					},
-					onAnimate: function(val){
-						domNode.scrollTop = val;
-					},
-					easing: easing.expoInOut, // TODO: IMPROVEME
-					duration: duration,
-					rate: 20 // TODO: IMPROVEME
-				});
-				self._animation = anim;
+				if (to.y) {
+					domNode.scrollTop = to.y;
+				}
+			} else {
+				var from = {
+					x: to.x ? domNode.scrollLeft : undefined,
+					y: to.y ? domNode.scrollTop : undefined
+                };
+				var animation = function () {
+					if (self._animation && self._animation.status() === "playing") {
+						self._animation.stop();
+					}
+					// dojo/_base/fx._Line cannot be used for animating several
+					// properties at once. Hence:
+					baseFx._Line.prototype.getValue = function (/*float*/ n) {
+						return {
+							x: ((this.end.x - this.start.x) * n) + this.start.x,
+							y: ((this.end.y - this.start.y) * n) + this.start.y
+						};
+					};
+					var	anim = new baseFx.Animation({
+						beforeBegin: function () {
+							if (this.curve) {
+								delete this.curve;
+							}
+							anim.curve = new baseFx._Line(from, to);
+						},
+						onAnimate: function (val) {
+							if (val.x) {
+								domNode.scrollLeft = val.x;
+							}
+							if (val.y) {
+								domNode.scrollTop = val.y;
+							}
+						},
+						easing: easing.expoInOut, // TODO: IMPROVEME
+						duration: duration,
+						rate: 20 // TODO: IMPROVEME
+					});
+					self._animation = anim;
 
-				return anim; // dojo/_base/fx/Animation
-			};
-			animation(to).play();
+					return anim; // dojo/_base/fx/Animation
+				};
+				animation().play();
+			}
 		}
 	});
 	
