96eec9986737756b30fd31ccdd258a5d8a82d2e5
Damien Mandrioli <dmandrioli@gmail.com>
2013-11-26 17:07:20 +0100

    MultiColumns WIP

--- MultiColumns.js
+++ MultiColumns.js
@@ -1,5 +1,6 @@
 define([
 	"dcl/dcl",
+	"dojo/query",
 	"./register",
 	"./Widget",
 	"./Container",
@@ -9,10 +10,48 @@ define([
 	"dojo/dom",
 	"dojo/dom-geometry",
 	"dojo/dom-class",
+	"dojo/dom-construct",
 	"./themes/load!MultiColumns"],
-	function(dcl, register, Widget, Container, Invalidating, BasicLayout, lang, dom, domGeom, domClass){
-		return register("d-multi-columns", BasicLayout, {
-			baseClass: "duiMultiColumns"
+	function(dcl, query, register, Widget, Container, Invalidating, BasicLayout, lang, dom, domGeom, domClass, domConstruct){
+		return register("d-multi-columns", [HTMLElement, Widget, Container, Invalidating], {
+			baseClass: "duiMultiColumns",
+			_bl: null,
+			preCreate: function () {
+				this.addInvalidatingProperties("test");
+			},
+
+			refreshRendering: function(){
+				if(this.direction == "horizontal"){
+					domClass.add(this, "mblHBasicLayout");
+					domClass.remove(this, "mblVBasicLayout");
+				}else{
+					domClass.add(this, "mblVBasicLayout");
+					domClass.remove(this, "mblHBasicLayout");
+				}
+				console.log("refresh");
+
+				query(".duiMultiColumns > .duiBasicLayout > *").forEach(function(node, index, arr){
+					domClass.add(node, "fill");
+				});
+
+			},
+			buildRendering: function(){
+				this._bl = new BasicLayout();
+				while (this.children.length > 0){
+					this._bl.addChild(this.children[0]);
+				}
+				domConstruct.place(this._bl, this);
+				console.log("build", this.children);
+				this.invalidateRendering();
+			},
+			addChild: dcl.superCall(function(sup){
+				return function (/*dui/Widget|DOMNode*/ widget, /*int?*/ insertIndex) {
+					sup.apply(this, arguments);
+
+					console.log("added", widget);
+				}})
+
+
 		})
 });
 
--- themes/bootstrap/MultiColumns.js
+++ themes/bootstrap/MultiColumns.js
@@ -1,17 +1,52 @@
 define(function(){ return '\
+.duiMultiColumns {\
+  overflow: hidden;\
+}\
 @media screen and (max-width: 501px) {\
-  .duiMultiColumns {\
+  .duiMultiColumns > .duiBasicLayout {\
     width: 500%;\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos1 {\
+    -webkit-transform: translate3d(-20%, 0px, 0px);\
+    transform: translate3d(-20%, 0px, 0px);\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos2 {\
+    -webkit-transform: translate3d(-40%, 0px, 0px);\
+    transform: translate3d(-40%, 0px, 0px);\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos3 {\
+    -webkit-transform: translate3d(-60%, 0px, 0px);\
+    transform: translate3d(-60%, 0px, 0px);\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos4 {\
+    -webkit-transform: translate3d(-80%, 0px, 0px);\
+    transform: translate3d(-80%, 0px, 0px);\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos5 {\
+    -webkit-transform: translate3d(-100%, 0px, 0px);\
+    transform: translate3d(-100%, 0px, 0px);\
   }\
 }\
 @media screen and (min-width: 501px) and (max-width: 801px) {\
-  .duiMultiColumns {\
+  .duiMultiColumns > .duiBasicLayout {\
     width: 125%;\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos1 {\
+    -webkit-transform: translate3d(-20%, 0px, 0px);\
+    transform: translate3d(-20%, 0px, 0px);\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos2 {\
+    -webkit-transform: translate3d(-40%, 0px, 0px);\
+    transform: translate3d(-40%, 0px, 0px);\
   }\
 }\
 @media screen and (min-width: 801px) {\
-  .duiMultiColumns {\
+  .duiMultiColumns > .duiBasicLayout {\
     width: 100%;\
+  }\
+  .duiMultiColumns > .duiBasicLayout.pos1 {\
+    -webkit-transform: translate3d(-20%, 0px, 0px);\
+    transform: translate3d(-20%, 0px, 0px);\
   }\
 }\
 '; } );
