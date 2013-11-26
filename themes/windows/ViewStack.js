42a3122dbec0cbbb26133958245e98a9fc766659
Adrian Vasiliu <vasiliu@fr.ibm.com>
2013-11-26 19:28:37 +0100

    Change ViewStack's styling such that it does not end up with overflow-y: auto (by side-effect of setting just overflow-x: hidden)

--- themes/windows/ViewStack.js
+++ themes/windows/ViewStack.js
@@ -16,12 +16,13 @@ define(function(){ return '\
 .duiViewStack {\
   box-sizing: border-box !important;\
   overflow-x: hidden !important;\
+  overflow-y: hidden;\
   position: relative !important;\
 }\
 .duiBasicLayout > .duiViewStack {\
   display: block !important;\
 }\
-.duiViewStack div {\
+.duiViewStack > * {\
   position: absolute !important;\
   box-sizing: border-box !important;\
   width: 100% !important;\
