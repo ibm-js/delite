6c57382e1874961fb27e379ba9669a6e8d059bad
Adrian Vasiliu <vasiliu@fr.ibm.com>
2013-11-26 19:14:52 +0100

    Now supports changing direction after creation (via Invalidating); inherits from HTMLElement; redesign of the scrollDirection API; added events for scroll to bottom/top.

--- ScrollableContainer.js
+++ ScrollableContainer.js
@@ -2,51 +2,88 @@ define([
 	"dcl/dcl",
 	"dojo/dom",
 	"dojo/dom-style",
+	"dojo/on",
 	"dojo/_base/fx",
 	"dojo/fx/easing",
 	"./register",
 	"./Widget",
 	"./Container",
+	"./Invalidating",
 	"./themes/load!ScrollableContainer"
-], function (dcl, dom, domStyle, baseFx, easing, register, Widget, Container) {
+], function (dcl, dom, domStyle, on, baseFx, easing, register, Widget, Container, Invalidating) {
 
 	// module:
 	//		dui/ScrollableContainer
-
-	var ScrollableContainer = dcl([Widget, Container], {
+	
+	return register("d-scrollable-container", [HTMLElement, Widget, Container, Invalidating], {
 		// summary:
 		//		A container widget with scrolling capabilities.
 		// description:
 		//		A container widget which can scroll its content 
-		//		horizontally and/or vertically.
+		//		horizontally and/or vertically. TODO: document events etc.
 
 		// scrollDirection: String
-		//		The direction of the interactive scroll:
-		//		v: vertical, h: horizontal, vh: both.
+		//		The direction of the interactive scroll. Possible values are:
+		//		"vertical", "horizontal", and "both". The default value is "vertical".
 		//		Note that scrolling programmatically using scrollTo() is
 		//		possible on both direction independently on the value of scrollDirection.
-		scrollDirection: "v",
+		scrollDirection: "vertical",
 		
 		// baseClass: String
 		//		The name of the CSS class of this widget.
 		baseClass: "duiScrollableContainer",
 		
+		preCreate: function () {
+			this.addInvalidatingProperties("scrollDirection");
+		},
+
+		refreshRendering: function(){
+			if (this.scrollDirection === "horizontal") {
+				domStyle.set(this.containerNode, "overflowX", "scroll");
+				domStyle.set(this.containerNode, "overflowY", ""); // restore the default
+			} else if (this.scrollDirection === "vertical") {
+				domStyle.set(this.containerNode, "overflowX", ""); // restore the default
+				domStyle.set(this.containerNode, "overflowY", "scroll");
+			} else if (this.scrollDirection === "both") {
+				domStyle.set(this.containerNode, "overflowX", "scroll");
+				domStyle.set(this.containerNode, "overflowY", "scroll");
+			} // else: do nothing for unsupported values
+		},
+
 		buildRendering: function () {
 			this.containerNode = this;
 			dom.setSelectable(this.containerNode, false);
+			this.invalidateRendering();
+			on(this.containerNode, "scroll", this._onScroll);
 			
-			if (this.scrollDirection.indexOf("v") !== -1) {
-				domStyle.set(this.containerNode, "overflowY", "scroll");
-			}
-			if (this.scrollDirection.indexOf("h") !== -1) {
-				domStyle.set(this.containerNode, "overflowX", "scroll");
-			}
+			/* TODO: Temporary, for testing, delete */
+			this.on("deliteful-scroll-bottom", function() {
+				console.log("deliteful-scroll-bottom");
+				alert("bottom");
+			});
+			this.on("deliteful-scroll-top", function() {
+				console.log("deliteful-scroll-top");
+				alert("top");
+			});
 		},
 		
-		// TODO: custom setter for scrollDirection? The difficult point is what
-		// value to restore for overflowX/Y when the user changes the scrollDirection
-		// afterwards. The default one? Or should we store the initial value such that
-		// we can restore it? Needs more thinking.
+		_onScroll: function(e) {
+			scroller = this.containerNode;
+			/* TODO: temporary, for debug, delete
+			console.log("left hand: " + (scroller.offsetHeight + scroller.scrollTop));
+			console.log("right hand: " + (scroller.scrollHeight));
+			console.log("diff: " + ((scroller.offsetHeight + scroller.scrollTop) - scroller.scrollHeight));
+			*/
+			// TODO: better event names?
+			// TODO: check taking into account margins, padding etc.
+			if (scroller.offsetHeight + scroller.scrollTop >= scroller.scrollHeight) {
+				// scrolled to bottoms
+				this.emit("deliteful-scroll-bottom");
+			} else if (scroller.scrollTop == 0) {
+				// scrolled to top
+				this.emit("deliteful-scroll-top");
+			}
+		},
 		
 		// TODO: to be tested if it's worth setting the overflow:scroll on this.containerNode
 		// versus setting it directly on this. The later would allow the user to install 
@@ -123,6 +160,4 @@ define([
 			}
 		}
 	});
-	
-	return register("d-scrollable-container", [HTMLElement, ScrollableContainer]);
 });
