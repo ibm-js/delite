cad1fa9fd87a1884060590a3dbe470d817edf4eb
Damien Mandrioli <dmandrioli@gmail.com>
2013-11-26 14:42:43 +0100

    draft of a first responsive multi column widget

--- themes/blackberry/MultiColumns.js
+++ themes/blackberry/MultiColumns.js
@@ -1,52 +1,17 @@
 define(function(){ return '\
-.duiMultiColumns {\
-  overflow: hidden;\
-}\
 @media screen and (max-width: 501px) {\
-  .duiMultiColumns > .duiBasicLayout {\
+  .duiMultiColumns {\
     width: 500%;\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos1 {\
-    -webkit-transform: translate3d(-20%, 0px, 0px);\
-    transform: translate3d(-20%, 0px, 0px);\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos2 {\
-    -webkit-transform: translate3d(-40%, 0px, 0px);\
-    transform: translate3d(-40%, 0px, 0px);\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos3 {\
-    -webkit-transform: translate3d(-60%, 0px, 0px);\
-    transform: translate3d(-60%, 0px, 0px);\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos4 {\
-    -webkit-transform: translate3d(-80%, 0px, 0px);\
-    transform: translate3d(-80%, 0px, 0px);\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos5 {\
-    -webkit-transform: translate3d(-100%, 0px, 0px);\
-    transform: translate3d(-100%, 0px, 0px);\
   }\
 }\
 @media screen and (min-width: 501px) and (max-width: 801px) {\
-  .duiMultiColumns > .duiBasicLayout {\
+  .duiMultiColumns {\
     width: 125%;\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos1 {\
-    -webkit-transform: translate3d(-20%, 0px, 0px);\
-    transform: translate3d(-20%, 0px, 0px);\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos2 {\
-    -webkit-transform: translate3d(-40%, 0px, 0px);\
-    transform: translate3d(-40%, 0px, 0px);\
   }\
 }\
 @media screen and (min-width: 801px) {\
-  .duiMultiColumns > .duiBasicLayout {\
+  .duiMultiColumns {\
     width: 100%;\
-  }\
-  .duiMultiColumns > .duiBasicLayout.pos1 {\
-    -webkit-transform: translate3d(-20%, 0px, 0px);\
-    transform: translate3d(-20%, 0px, 0px);\
   }\
 }\
 '; } );
