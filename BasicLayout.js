22ec29c4e498eaa6903d8c6749ab98933db7c57e
Christophe Jolif <cjolif@gmail.com>
2013-11-29 13:57:10 +0100

    Squashed commit of the following:
    commit 5f48915bf53dc7ed421aada29d0beea9e9d4413a
    Author: Christophe Jolif <cjolif@gmail.com>
    Date:   Fri Nov 29 11:47:08 2013 +0100
    
        display-event architecture: display container, controller & make sure BasicLayout inherits from it

--- BasicLayout.js
+++ BasicLayout.js
@@ -1,32 +1,32 @@
 define([
 	"./register",
 	"./Widget",
-	"./Container",
+	"./DisplayContainer",
 	"./Invalidating",
 	"dojo/dom-class",
-	"./themes/load!BasicLayout"],
-	function(register, Widget, Container, Invalidating, domClass){
-		return register("d-basic-layout", [HTMLElement, Widget, Container, Invalidating], {
+	"./themes/load!BasicLayout"
+], function (register, Widget, DisplayContainer, Invalidating, domClass) {
+	return register("d-basic-layout", [HTMLElement, Widget, DisplayContainer, Invalidating], {
 
-			baseClass: "duiBasicLayout",
-			direction: "horizontal",
+		baseClass: "duiBasicLayout",
+		direction: "horizontal",
 
-			preCreate: function () {
-				this.addInvalidatingProperties("direction");
-			},
+		preCreate: function () {
+			this.addInvalidatingProperties("direction");
+		},
 
-			refreshRendering: function(){
-				if(this.direction == "horizontal"){
-					domClass.add(this, "mblHBasicLayout");
-					domClass.remove(this, "mblVBasicLayout");
-				}else{
-					domClass.add(this, "mblVBasicLayout");
-					domClass.remove(this, "mblHBasicLayout");
-				}
-			},
-			buildRendering: function(){
-				this.invalidateRendering();
+		refreshRendering: function () {
+			if (this.direction === "horizontal") {
+				domClass.add(this, "mblHBasicLayout");
+				domClass.remove(tjhis, "mblVBasicLayout");
+			} else {
+				domClass.add(this, "mblVBasicLayout");
+				domClass.remove(this, "mblHBasicLayout");
 			}
-		});
+		},
 
-	})
+		buildRendering: function () {
+			this.invalidateRendering();
+		}
+	});
+});
