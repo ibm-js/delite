c672d5666c5e13c375a540f68bdb697dcb4cc1f9
Damien Mandrioli <dmandrioli@gmail.com>
2013-12-02 17:54:45 +0100

    fix #10

--- ViewStack.js
+++ ViewStack.js
@@ -16,6 +16,20 @@ define(
 		"./themes/load!./themes/common/transitions/revealv",
 		"./themes/load!./themes/common/transitions/scaleIn"],
 	function (dcl, register, sniff, Widget, Container, Invalidating, lang, dom, domGeom, domClass) {
+		function setVisibility(node, val) {
+			if (val) {
+				node.style.visibility = "visible";
+				node.style.display = "";
+			} else {
+				node.style.visibility = "hidden";
+				node.style.display = "none";
+			}
+		}
+
+		function transitionClass(s) {
+			return "dui" + s.charAt(0).toUpperCase() + s.substring(1);
+		}
+
 		return register("d-view-stack", [HTMLElement, Widget, Container, Invalidating], {
 
 			// summary:
@@ -46,20 +60,23 @@ define(
 			transitionTiming: {default: 0, ios: 20, android: 100, mozilla: 100},
 
 			_timing: 0,
+			_visibleChild: null,
+			_transitionEndHandlers: [],
 
 			preCreate: function () {
 				this.addInvalidatingProperties("transitionTiming");
 			},
+
 			buildRendering: function () {
 				for (var i = 1; i < this.children.length; i++) {
-					this._setVisibility(this.children[i], false);
+					setVisibility(this.children[i], false);
 				}
 				this.invalidateRendering();
 			},
 
 			refreshRendering: function () {
 				for (var o in this.transitionTiming){
-				    if (sniff(o) && this._timing < this.transitionTiming[o]){
+					if (sniff(o) && this._timing < this.transitionTiming[o]){
 						this._timing = this.transitionTiming[o];
 					}
 				}
@@ -103,11 +120,11 @@ define(
 						if (!props.transition) {
 							props.transition = this.transition;
 						}
-						this._setVisibility(node, true);
+						setVisibility(node, true);
 						this._setAfterTransitionHandlers(origin, props);
 						this._setAfterTransitionHandlers(node, props);
-						domClass.add(origin, this._transitionClass(props.transition));
-						domClass.add(node, this._transitionClass(props.transition));
+						domClass.add(origin, transitionClass(props.transition));
+						domClass.add(node, transitionClass(props.transition));
 						domClass.remove(node, "duiTransition");
 						domClass.add(node, "duiIn");
 						if (props.reverse === true) {
@@ -134,35 +151,15 @@ define(
 			addChild: dcl.superCall(function (sup) {
 				return function (/*dui/Widget|DOMNode*/ widget, /*jshint unused: vars */insertIndex) {
 					sup.apply(this, arguments);
-					this._setVisibility(widget, false);
+					setVisibility(widget, false);
 				};
 			}),
 
-			_visibleChild: null,
-
-			_transitionEndHandlers: [],
-
 			_setAfterTransitionHandlers: function (node, props) {
-
 				var handle = lang.hitch(this, this._afterTransitionHandle);
 				this._transitionEndHandlers.push({node: node, handle: handle, props: props});
 				node.addEventListener("webkitTransitionEnd", handle);
 				node.addEventListener("transitionend", handle); // IE10 + FF
-
-			},
-
-			_setVisibility: function (node, val) {
-				if (val) {
-					node.style.visibility = "visible";
-					node.style.display = "";
-				} else {
-					node.style.visibility = "hidden";
-					node.style.display = "none";
-				}
-			},
-
-			_transitionClass: function (s) {
-				return "dui" + s.charAt(0).toUpperCase() + s.substring(1);
 			},
 
 			_afterTransitionHandle: function (event) {
@@ -171,12 +168,12 @@ define(
 					item = this._transitionEndHandlers[i];
 					if (event.target === item.node) {
 						if (domClass.contains(item.node, "duiOut")) {
-							this._setVisibility(item.node, false);
+							setVisibility(item.node, false);
 						}
 						domClass.remove(item.node, "duiIn");
 						domClass.remove(item.node, "duiOut");
 						domClass.remove(item.node, "duiReverse");
-						domClass.remove(item.node, this._transitionClass(item.props.transition));
+						domClass.remove(item.node, transitionClass(item.props.transition));
 						domClass.remove(item.node, "duiTransition");
 						item.node.removeEventListener("webkitTransitionEnd", item.handle);
 						item.node.removeEventListener("transitionend", item.handle);
