14c9b02df9bfc8b123252e8bb63adf3629c3b3a2
Christophe Jolif <cjolif@gmail.com>
2014-01-08 16:50:27 +0100

    enable requirejs for unit tests

--- tests/client.js
+++ tests/client.js
@@ -1,10 +1,15 @@
 // Test file to run infrastructure tests from a browser
-// Run using http://localhost/delite/node_modules/intern/client.html?config=tests/infrastructure/client
+// Run using http://localhost/delite/node_modules/intern/client.html?config=tests/client
 
 define({
 	loader: {
 		// location of all the packages, relative to client.html
 		baseUrl: "../../.."
+	},
+
+	useLoader: {
+		"host-node": "requirejs",
+		"host-browser": "../../../requirejs/require.js"
 	},
 
 	// Non-functional test suites
