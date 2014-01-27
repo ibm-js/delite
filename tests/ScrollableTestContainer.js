66a3f9b4e9c98faadde3422264509ac8f91150a4
Adrian Vasiliu <vasiliu@fr.ibm.com>
2014-01-27 12:53:03 +0100

    Fix tests: ensure assertions are checked after widget's refreshRendering(), thus fixing errors on IE10; remove useless inherintance from delite/Widget in test container, now that Scrollable extends Widget.

--- tests/ScrollableTestContainer.js
+++ tests/ScrollableTestContainer.js
@@ -1,15 +1,14 @@
 define([
 	"delite/register",
-	"delite/Widget",
 	"delite/Container",
 	"delite/Scrollable"
-], function (register, Widget, Container, Scrollable) {
+], function (register, Container, Scrollable) {
 
 	// The purpose of this module is to provide a concrete widget using
 	// the mixin delite/Scrollable in order to ease the testing of the mixin itself.
 	
 	return register("test-scrollable-container", 
-		[HTMLElement, Widget, Container, Scrollable], {
+		[HTMLElement, Container, Scrollable], {
 		baseClass: "test-scrollable-container"
 	});
 });
