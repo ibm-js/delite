05fcb5fa0fd1068cd7898319470a47f35c58cda8
Bill Keese <bill@dojotoolkit.org>
2014-01-29 17:12:40 +0900

    Fix KeyNav test on IE10, IE11. Tabbing from the last element on the page does not necessarily go to the first element. It might go to the address bar instead.
    Also fix order of params to assert(), and add hints so we can tell which assert failed.

--- tests/KeyNav.js
+++ tests/KeyNav.js
@@ -19,59 +19,39 @@ define(["intern!object",
 			}
 			return this.remote.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("autofocusInput", value);
+						assert.equal(value, "autofocusInput", "initial element");
 					})
 				.keys("\uE004") // tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal(value, "one", "tab to one");
 					})
 				.keys("\uE008\uE004") // shift tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("autofocusInput", value);
+						assert.equal(value, "autofocusInput", "shift tab back to autofocusInput");
 					})
 				.keys("\uE008") // release shift
 				.keys("\uE004") // tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal(value, "one", "tab to one again");
 					})
 				.keys("\uE004") // tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("emptyContainer", value);
+						assert.equal(value, "emptyContainer", "tab to emptyContainer");
 					})
 				.keys("\uE008\uE004") // shift tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal(value, "one", "shift tab to one");
 					})
 				.keys("\uE008") // release shift
 				.keys("\uE004") // tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("emptyContainer", value);
-					})
-				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("autofocusInput", value);
-					})
-				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("one", value);
-					})
-				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("emptyContainer", value);
-					})
-				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("autofocusInput", value);
+						assert.equal(value, "emptyContainer", "tab to emptyContainer again");
 					});
 		},
 		"intra widget arrow navigation": function () {
@@ -79,66 +59,66 @@ define(["intern!object",
 				// SafariDriver doesn't support tabbing, see https://code.google.com/p/selenium/issues/detail?id=5403
 				return;
 			}
-			return this.remote.execute("return document.activeElement.id")
+			return this.remote.execute("autofocusInput.focus();")
+				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("autofocusInput", value);
+						assert.equal(value, "autofocusInput", "start on autofocusInput");
 					})
 				.keys("\uE004") // tab
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal(value, "one", "tabbed to one");
 					})
 				.keys("\uE015") // arrow down
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("two", value);
+						assert.equal(value, "two", "arrowed to two");
 					})
 				.keys("\uE015") // arrow down
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("three", value);
+						assert.equal(value, "three", "arrowed to three");
 					})
 				.keys("\uE015") // arrow down
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("four", value);
+						assert.equal(value, "four", "arrowed to four");
 					})
 				.keys("\uE015") // arrow down
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("five", value);
+						assert.equal(value, "five", "arrowed to five");
 					})
 				.keys("\uE015") // arrow down
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("five", value);
+						assert.equal(value, "five", "still on five");
 					})
 				.keys("\uE013") // arrow up
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("four", value);
+						assert.equal(value, "four", "back to four");
 					})
 				.keys("\uE013") // arrow up
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("three", value);
+						assert.equal(value, "three", "back to three");
 					})
 				.keys("\uE013") // arrow up
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("two", value);
+						assert.equal("two", value, "back to two");
 					})
 				.keys("\uE013") // arrow up
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal("one", value, "back to one");
 					})
 				.keys("\uE013") // arrow up
 				.execute("return document.activeElement.id")
 					.then(function (value) {
-						assert.equal("one", value);
+						assert.equal("one", value, "still on one");
 					});
 		}
-
 	});
 });
