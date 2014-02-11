bf1d7ebe87ece101525de39bddbc55e2efc59ed7
Bill Keese <bill@dojotoolkit.org>
2014-02-11 21:44:56 +0900

    KeyNav: fix tabIndex and other problems, and write proper tests.
    Don't put tabIndex in the KeyNav prototype.  It triggers Stateful to create a custom
    tabIndex setter which then triggers Widget#enteredViewCallback to
    call the setter on widget startup.  We don't want any of these things to happen.
    Rather, the app should be able to set/change tabIndex on the element (i.e. widget
    root node) without any interference/action from delite.
    
    The only thing that we really need to do is to set tabIndex to 0 if it was not
    specified explicitly by the app.
    
    The test changes include:
    
    - test letter key search
    - test left, right, home, and end key callbacks
    - test programmatic creation
    - test childSelector as string and as function
    - more extensive tabindex checks
    
    Fixes #62.

--- tests/functional/KeyNav.js
+++ tests/functional/KeyNav.js
@@ -12,115 +12,130 @@ define(["intern!object",
 				.waitForCondition("ready", 10000);
 		},
 
-		"inter widget tab navigation": function () {
+		"tabindex": function () {
 			if (/safari|iPhone/.test(this.remote.environmentType.browserName)) {
 				// SafariDriver doesn't support tabbing, see https://code.google.com/p/selenium/issues/detail?id=5403
 				return;
 			}
-			return this.remote.execute("return document.activeElement.id")
+			return this.remote.execute("return document.activeElement.value")
 					.then(function (value) {
-						assert.equal(value, "autofocusInput", "initial element");
+						assert.equal(value, "auto focus", "initial element");
 					})
 				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
+				.execute("return document.activeElement.textContent")
 					.then(function (value) {
-						assert.equal(value, "one", "tab to one");
+						assert.equal(value, "apple", "tabbed to apple");
 					})
 				.keys("\uE008\uE004") // shift tab
-				.execute("return document.activeElement.id")
+				.execute("return document.activeElement.value")
 					.then(function (value) {
-						assert.equal(value, "autofocusInput", "shift tab back to autofocusInput");
+						assert.equal(value, "auto focus", "shift tabbed back to initial element");
 					})
 				.keys("\uE008") // release shift
 				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
+				.execute("return document.activeElement.textContent")
 					.then(function (value) {
-						assert.equal(value, "one", "tab to one again");
+						assert.equal(value, "apple", "tabbed to apple again");
 					})
 				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "emptyContainer", "tab to emptyContainer");
-					})
-				.keys("\uE008\uE004") // shift tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "one", "shift tab to one");
-					})
-				.keys("\uE008") // release shift
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "one", "tabbed to first element on programmatic KeyNav w/implicit tabIndex");
+				})
+				.execute("secondInput.focus()")// don't tab from previous KeyNav, it goes to address bar [on chrome]
+				.execute("return document.activeElement.value")
+				.then(function (value) {
+					assert.equal(value, "tabindex=2", "focused input before prog KeyNav w/tabindex=3 setting");
+				})
 				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "four", "tabbed to declarative KeyNav with tabindex=3 setting");
+				})
+				.keys("\uE004") // tab
+				.keys("\uE004") // tab
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "seven", "tabbed past INPUT to programmatic KeyNav with tabindex=5 setting");
+				})
+
+		},
+		"arrow navigation": function () {
+			if (/safari|iPhone/.test(this.remote.environmentType.browserName)) {
+				// SafariDriver apparently doesn't support arrow keys either
+				return;
+			}
+			return this.remote.execute("grid.focus();")
+				.execute("return document.activeElement.textContent")
 					.then(function (value) {
-						assert.equal(value, "emptyContainer", "tab to emptyContainer again");
+						assert.equal(value, "apple", "tab");
+					})
+				.keys("\uE015") // arrow down
+				.execute("return document.activeElement.textContent")
+					.then(function (value) {
+						assert.equal(value, "pear", "down");
+					})
+				.keys("\uE014") // arrow right
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "grapes", "right");
+				})
+				.keys("\uE012") // arrow left
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "pear", "left");
+				})
+				.keys("\uE013") // arrow up
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "apple", "up");
+				})
+				.keys("\uE010") // end
+				.execute("return document.activeElement.textContent")
+					.then(function (value) {
+						assert.equal(value, "raspberry", "end");
+					})
+				.keys("\uE011") // home
+				.execute("return document.activeElement.textContent")
+					.then(function (value) {
+						assert.equal(value, "apple", "home");
 					});
 		},
-		"intra widget arrow navigation": function () {
+
+		"letter search": function () {
 			if (/safari|iPhone/.test(this.remote.environmentType.browserName)) {
-				// SafariDriver doesn't support tabbing, see https://code.google.com/p/selenium/issues/detail?id=5403
+				// SafariDriver just doesn't work testing keystrokes ...
 				return;
 			}
-			return this.remote.execute("autofocusInput.focus();")
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "autofocusInput", "start on autofocusInput");
-					})
-				.keys("\uE004") // tab
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "one", "tabbed to one");
-					})
-				.keys("\uE015") // arrow down
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "two", "arrowed to two");
-					})
-				.keys("\uE015") // arrow down
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "three", "arrowed to three");
-					})
-				.keys("\uE015") // arrow down
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "four", "arrowed to four");
-					})
-				.keys("\uE015") // arrow down
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "five", "arrowed to five");
-					})
-				.keys("\uE015") // arrow down
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "five", "still on five");
-					})
-				.keys("\uE013") // arrow up
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "four", "back to four");
-					})
-				.keys("\uE013") // arrow up
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal(value, "three", "back to three");
-					})
-				.keys("\uE013") // arrow up
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("two", value, "back to two");
-					})
-				.keys("\uE013") // arrow up
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("one", value, "back to one");
-					})
-				.keys("\uE013") // arrow up
-				.execute("return document.activeElement.id")
-					.then(function (value) {
-						assert.equal("one", value, "still on one");
-					});
+			return this.remote.execute("grid.focus();")
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "apple", "tab");
+				})
+				.keys("b") // search for word starting with b
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "banana", "b");
+				})
+				.wait(1000)		// clear timer so next keystroke taken as new search rather than continuation
+				.keys("b") // search for next word starting with b
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "blueberry", "b again");
+				})
+				.wait(1000)		// clear timer so next keystroke taken as new search rather than continuation
+				.keys("r") // search for word starting with r
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "raspberry", "right");
+				})
+				.wait(1000)		// clear timer so next keystroke taken as new search rather than continuation
+				.keys("bl") // search for word starting with bl
+				.execute("return document.activeElement.textContent")
+				.then(function (value) {
+					assert.equal(value, "blueberry", "bl");
+				});
 		}
 
-		// TODO: test keyboard navigation
 	});
 });
