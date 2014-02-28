2353f9737ae926b658e1f4d169b0b1fe5d914000
Patrick Ruzand <pruzand@gmail.com>
2014-02-28 12:09:51 +0100

    duiFoo->d-foo class renaming

--- themes/bootstrap/common_css.js
+++ themes/bootstrap/common_css.js
@@ -1,5 +1,5 @@
 define(function(){ return '\
-.duiReset {\
+.d-reset {\
   margin: 0;\
   border: 0;\
   padding: 0;\
@@ -7,33 +7,33 @@ define(function(){ return '\
   line-height: normal;\
   color: inherit;\
 }\
-.dj_a11y .duiReset {\
+.dj_a11y .d-reset {\
   -moz-appearance: none;\
 }\
-.duiInline {\
+.d-inline {\
   display: inline-block;\
   border: 0;\
   padding: 0;\
   vertical-align: middle;\
 }\
-table.duiInline {\
+table.d-inline {\
   display: inline-table;\
   box-sizing: content-box;\
   -moz-box-sizing: content-box;\
 }\
-.duiHidden {\
+.d-hidden {\
   display: none !important;\
 }\
-.duiVisible {\
+.d-visible {\
   display: block !important;\
   position: relative;\
 }\
-.duiOffScreen {\
+.d-offscreen {\
   position: absolute !important;\
   left: -10000px !important;\
   top: -10000px !important;\
 }\
-.duiBackgroundIframe {\
+.d-background-iframe {\
   position: absolute;\
   left: 0;\
   top: 0;\
@@ -44,48 +44,48 @@ table.duiInline {\
   padding: 0;\
   margin: 0;\
 }\
-.duiContainer {\
+.d-container {\
   overflow: hidden;\
 }\
-.dj_a11y .duiIcon,\
-.dj_a11y div.duiArrowButtonInner,\
-.dj_a11y span.duiArrowButtonInner,\
-.dj_a11y img.duiArrowButtonInner {\
+.dj_a11y .d-icon,\
+.dj_a11y div.d-arrow-button-inner,\
+.dj_a11y span.d-arrow-button-inner,\
+.dj_a11y img.d-arrow-button-inner {\
   display: none;\
 }\
-.dj_a11y .duiA11ySideArrow {\
+.dj_a11y .d-a11y-side-arrow {\
   display: inline !important;\
   cursor: pointer;\
 }\
-.duiLayoutContainer {\
+.d-layout-container {\
   position: relative;\
   display: block;\
   overflow: hidden;\
 }\
-.duiAlignTop,\
-.duiAlignBottom,\
-.duiAlignLeft,\
-.duiAlignRight {\
+.d-align-top,\
+.d-align-bottom,\
+.d-align-left,\
+.d-align-right {\
   position: absolute;\
   overflow: hidden;\
 }\
-body .duiAlignClient {\
+body .d-align-client {\
   position: absolute;\
 }\
-.duiNoIcon {\
+.d-noicon {\
   display: none;\
 }\
-.duiReadOnly *,\
-.duiDisabled *,\
-.duiReadOnly,\
-.duiDisabled {\
+.d-readonly *,\
+.d-disabled *,\
+.d-readonly,\
+.d-disabled {\
   cursor: default;\
 }\
-.dj_gecko .duiArrowButtonInner INPUT,\
-.dj_gecko INPUT.duiArrowButtonInner {\
+.dj_gecko .d-arrow-button-inner INPUT,\
+.dj_gecko INPUT.d-arrow-button-inner {\
   -moz-user-focus: ignore;\
 }\
-.duiPopup {\
+.d-popup {\
   position: absolute;\
   background-color: transparent;\
   margin: 0;\
@@ -96,15 +96,15 @@ body .duiAlignClient {\
   -moz-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);\
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);\
 }\
-.duiTooltipDialogPopup {\
+.d-tooltip-dialog-popup {\
   -webkit-box-shadow: none;\
   -moz-box-shadow: none;\
   box-shadow: none;\
 }\
-.duiComboBoxHighlightMatch {\
+.d-combobox-highlight-match {\
   background-color: #abd6ff;\
 }\
-.duiFocusedLabel {\
+.d-focused-label {\
   outline: 1px dotted #494949;\
 }\
 html.mobile,\
@@ -120,47 +120,47 @@ html.mobile,\
   font-size: 17px;\
   color: #000000;\
 }\
-.duiBackground {\
+.d-background {\
   background-color: #c0c0c0;\
 }\
-.duiColorBlue {\
+.d-color-blue {\
   color: #ffffff;\
   background-color: #048bf4;\
   background-image: -webkit-gradient(linear, left top, left bottom, from(#48adfc), to(#048bf4));\
   background-image: linear-gradient(to bottom, #48adfc 0%, #048bf4 100%);\
 }\
-.duiColorBlue45 {\
+.d-color-blue-45 {\
   background-image: -webkit-gradient(linear, left top, right bottom, from(#048bf4), to(#48adfc));\
   background-image: linear-gradient(to right bottom, #048bf4 0%, #48adfc 100%);\
 }\
-.duiColorDefault {\
+.d-default-color {\
   color: #000000;\
   background-color: #a4a4a4;\
   background-image: -webkit-gradient(linear, left top, left bottom, from(#e2e2e2), to(#a4a4a4));\
   background-image: linear-gradient(to bottom, #e2e2e2 0%, #a4a4a4 100%);\
 }\
-.duiColorDefault45 {\
+.d-default-color-45 {\
   background-image: -webkit-gradient(linear, left top, right bottom, from(#e2e2e2), to(#a4a4a4));\
   background-image: linear-gradient(to right bottom, #e2e2e2 0%, #a4a4a4 100%);\
 }\
-.duiColorDefaultSel {\
+.d-default-color-sel {\
   color: #ffffff;\
   background-color: #999999;\
   background-image: -webkit-gradient(linear, left top, left bottom, from(#bbbbbb), to(#999999));\
   background-image: linear-gradient(to bottom, #bbbbbb 0%, #999999 100%);\
 }\
-.duiColorDefaultSel45 {\
+.d-default-color-sel-45 {\
   background-image: -webkit-gradient(linear, left top, right bottom, from(#bbbbbb), to(#999999));\
   background-image: linear-gradient(to right bottom, #bbbbbb 0%, #999999 100%);\
 }\
-.duiSpriteIcon {\
+.d-sprite-icon {\
   position: absolute;\
 }\
-.duiSpriteIconParent {\
+.d-sprite-icon-parent {\
   position: relative;\
   font-size: 1px;\
 }\
-.duiImageIcon {\
+.d-image-icon {\
   vertical-align: top;\
 }\
 '; } );
