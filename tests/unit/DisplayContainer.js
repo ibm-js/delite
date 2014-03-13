c3ca10631af4330b3231df3d28ef37d1828e398b
Christophe Jolif <cjolif@gmail.com>
2014-03-13 18:30:53 +0100

    add a new event to warn possible controller that we are going to transition, add a hide() method counterpart to the show() method.

--- tests/unit/DisplayContainer.js
+++ tests/unit/DisplayContainer.js
@@ -18,7 +18,7 @@ define([
 		},
 		// test with no controller
 		original: function () {
-			this.timeout = 5000;
+			this.timeout = 2500;
 			var deferred = new Deferred();
 			register("test-default-display-container", [HTMLElement, Widget, DisplayContainer]);
 			var dcontainer = register.createElement("test-default-display-container");
@@ -44,7 +44,7 @@ define([
 				testView(view1);
 				// by id
 				// test that a delite-display-complete event is fired, if is not fired the test will fail by timeout
-				on.once("delite-display-complete", function () {
+				on.once(dcontainer, "delite-display-complete", function () {
 					// test is finished
 					deferred.resolve(true);
 				});
@@ -55,16 +55,88 @@ define([
 			});
 			return deferred.promise;
 		},
+		hide: function () {
+			this.timeout = 2500;
+			var deferred = new Deferred();
+			register("test-hide-display-container", [HTMLElement, Widget, DisplayContainer]);
+			var dcontainer = register.createElement("test-hide-display-container");
+			function initView(view) {
+				dcontainer.appendChild(view);
+				view.style.visibility = "visible";
+				view.style.display = "";
+			}
+			function testView(view) {
+				assert.strictEqual(view.style.visibility, "hidden", "visibility");
+				assert.strictEqual(view.style.display, "none", "display");
+			}
+			var view1 = document.createElement("div");
+			initView(view1);
+			var view2 = document.createElement("div");
+			view2.setAttribute("id", "view");
+			initView(view2);
+			container.appendChild(dcontainer);
+			dcontainer.startup();
+			// by node
+			var transitionDeferred = dcontainer.hide(view1);
+			transitionDeferred.then(function () {
+				testView(view1);
+				// by id
+				// test that a delite-display-complete event is fired, if is not fired the test will fail by timeout
+				on.once(dcontainer, "delite-display-complete", function () {
+					// test is finished
+					deferred.resolve(true);
+				});
+				transitionDeferred = dcontainer.show("view");
+				transitionDeferred.then(function () {
+					testView(view2);
+				});
+			});
+			return deferred.promise;
+		},
+		event: function () {
+			this.timeout = 2500;
+			var deferred = new Deferred(), handler;
+			function initView(view, id) {
+				view.style.visibility = "hidden";
+				view.style.display = "none";
+				view.setAttribute("id", id + "-event");
+			}
+			document.addEventListener("delite-display-load", handler = function (event) {
+				event.preventDefault();
+				var view = document.createElement("div");
+				initView(view, event.dest);
+				event.loadDeferred.resolve({
+					child: view
+				});
+			});
+			register("test-event-display-container", [HTMLElement, Widget, DisplayContainer]);
+			var dcontainer = register.createElement("test-event-display-container");
+			dcontainer.setAttribute("id", "dcontainer");
+			container.appendChild(dcontainer);
+			dcontainer.startup();
+			var beforeDisplayCalled = false;
+			dcontainer.on("delite-display-before", function () {
+				beforeDisplayCalled = true;
+			});
+			// by node
+			var transitionDeferred = dcontainer.show("view1-event");
+			transitionDeferred.then(function () {
+				assert(beforeDisplayCalled, "added event must be dispatched");
+				document.removeEventListener("delite-display-load", handler);
+				deferred.resolve(true);
+			});
+			return deferred.promise;
+		},
 		// test with a controller
 		custom : function () {
-			this.timeout = 5000;
-			var deferred = new Deferred();
+			this.timeout = 2500;
+			var deferred = new Deferred(), handler;
 			function initView(view, id) {
 				view.style.visibility = "hidden";
 				view.style.display = "none";
 				view.setAttribute("id", id);
 			}
-			document.addEventListener("delite-display-load", function (event) {
+			document.addEventListener("delite-display-load", handler = function (event) {
 				event.preventDefault();
 				var view = document.createElement("div");
 				initView(view, event.dest);
@@ -89,6 +161,7 @@ define([
 				transitionDeferred = dcontainer.show("view2");
 				transitionDeferred.then(function () {
 					testView(document.getElementById("view2"));
+					document.removeEventListener("delite-display-load", handler);
 					// test is finished
 					deferred.resolve(true);
 				});
