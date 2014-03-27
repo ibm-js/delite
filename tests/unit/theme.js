62437826e9c2eb79f1e471f63994925ecf35c347
Sebastien Pereira <spereira@javalive.net>
2014-03-27 15:30:03 +0100

    Theme refactoring.
    * delite/themes/{{theme}}/global.less
      -- defines tags selectors + custom CSS classes for the theme
      -- imports delite/themes/common/global.less (custom CSS classes common to all themes)
    * removed common_template
      -- use theme/global.less or common/global.less
    * renamed css3.less ==>  mixins.less
    * transparently load {{theme}}/global file of the current theme: widgets don't have anymore to explicitly ask the loader to load common theme CSS definitions.
    * fix regression test failures from 6b587f9cac7b7cdfbf30341d85f85dca4141c7c9
    * fix popup test on iOS: increased height to make sure buttons are visible
    
    Fixes #134, #137.

---
+++ tests/unit/theme.js
@@ -0,0 +1,30 @@
+define([
+	"require",
+	"intern!object",
+	"intern/chai!assert",
+	"dojo/dom-style"
+], function (require, registerSuite, assert, domStyle) {
+	
+	function getStyles() {
+		// summary:
+		//		Debugging method to get all the styles in the document.
+		return Array.prototype.map.call(document.getElementsByTagName("style"),function (s) {
+			return "<style>\n" + s.innerHTML.substr(0, 100) + "\n</style>";
+		}).join("\n");
+	}
+	
+	registerSuite({
+		name: "theme",
+		"load global_css": function () {
+			var d = new this.async(1000);
+			// Load one module that use delite/themes/load! to load global_css
+			require([
+				"./resources/TestThemeWidget1"
+			], d.callback(function () {
+				// global_css should be automatically loaded. It defines class d-reset (themes/common/global.less)
+				assert.strictEqual(getStyles().match(/d-reset/g).length, 1, "global_css is loaded");
+			}));
+			return d;
+		}
+	});
+});
