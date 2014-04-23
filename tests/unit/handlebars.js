4aa3d03784002a938e8bace6889f39021383329c
Bill Keese <bill@dojotoolkit.org>
2014-04-23 18:26:23 +0900

    make sure widget names are unique

--- tests/unit/handlebars.js
+++ tests/unit/handlebars.js
@@ -18,7 +18,7 @@ define([
 		},
 		load: function () {
 			// Test that function returned from delite/handlebars! creates the template correctly
-			var TestButton = register("test-button", [HTMLButtonElement, Widget], {
+			var TestButton = register("handlebars-button", [HTMLButtonElement, Widget], {
 				iconClass: "originalClass",
 				label: "original label",
 				buildRendering: simpleHBTmpl
@@ -42,7 +42,7 @@ define([
 		"data-attach-point": function () {
 			// Testing that data-attach-point works
 
-			var TestHtml = register("test-data-attach-point", [HTMLElement, Widget], {
+			var TestHtml = register("handlebars-data-attach-point", [HTMLElement, Widget], {
 				buildRendering: handlebars.compile(
 						"<template data-attach-point='root,root2'>" +
 						"<button data-attach-point='myButton, myButton2'>hi</button>" +
@@ -52,14 +52,14 @@ define([
 
 			var node = new TestHtml();
 
-			assert.strictEqual(node.root.tagName.toLowerCase(), "test-data-attach-point", "node.root");
-			assert.strictEqual(node.root2.tagName.toLowerCase(), "test-data-attach-point", "node.root");
+			assert.strictEqual(node.root.tagName.toLowerCase(), "handlebars-data-attach-point", "node.root");
+			assert.strictEqual(node.root2.tagName.toLowerCase(), "handlebars-data-attach-point", "node.root");
 			assert.strictEqual(node.myButton.tagName.toLowerCase(), "button", "node.myButton");
 			assert.strictEqual(node.myButton2.tagName.toLowerCase(), "button", "node.myButton2");
 		},
 
 		branching: function () {
-			var NoLabelButton = register("no-label-button", [HTMLButtonElement, Widget], {
+			var NoLabelButton = register("handlebars-no-label-button", [HTMLButtonElement, Widget], {
 				iconClass: "originalClass",
 				showLabel: false,
 				label: "original label",
@@ -74,7 +74,7 @@ define([
 			assert.strictEqual(myButton.textContent.trim(), "", "still no label");
 
 			// Test true if condition
-			var LabelButton = register("label-button", [HTMLButtonElement, Widget], {
+			var LabelButton = register("handlebars-label-button", [HTMLButtonElement, Widget], {
 				iconClass: "originalClass",
 				showLabel: true,
 				label: "original label",
@@ -93,7 +93,7 @@ define([
 		},
 
 		"special props": function () {
-			var SpecialPropsWidget = register("special-props", [HTMLElement, Widget], {
+			var SpecialPropsWidget = register("handlebars-special-props", [HTMLElement, Widget], {
 				inputClass: "originalClass",	// attribute called "class" but property called "className"
 				inputValue: "original value",	// must be set as property
 				role: "originalRole",			// must be set as attribute
@@ -124,7 +124,7 @@ define([
 
 		"special characters": function () {
 			// Test that special characters are escaped.  This is actually testing template.js.
-			var TestList = register("test-ul", [HTMLUListElement, Widget], {
+			var TestList = register("handlebars-ul", [HTMLUListElement, Widget], {
 				label: "bill'\\",
 				buildRendering: handlebars.compile(
 					"<ul><li foo=\"a.b('c,d')\" bar='\\\"hello\"'>\"\\{{label}}\n\twas \n\there'</li></ul>")
@@ -141,7 +141,7 @@ define([
 			// Test that listeners like onclick work.
 			/* global g:true */
 			g = 1;
-			var TestClick = register("test-events", [ HTMLElement, Widget], {
+			var TestClick = register("handlebars-events", [ HTMLElement, Widget], {
 				buildRendering: handlebars.compile(
 					"<template><span onclick='g = 2;'>click me</span></template>")
 			});
@@ -152,27 +152,27 @@ define([
 		},
 
 		"widgets in templates": function () {
-			register("simple-heading", [HTMLElement, Widget], {
+			register("handlebars-heading", [HTMLElement, Widget], {
 				text: "",
-				buildRendering: handlebars.compile("<simple-heading>{{text}}</simple-heading>")
+				buildRendering: handlebars.compile("<handlebars-heading>{{text}}</handlebars-heading>")
 			});
 
-			// This widget uses sub-widgets test-button (defined in first test) and also simple-heading.
-			var ComplexWidget = register("widgets-in-template", [HTMLElement, Widget], {
+			// This widget uses sub-widgets handlebars-button (defined in first test) and also handlebars-heading.
+			var ComplexWidget = register("handlebars-widgets-in-template", [HTMLElement, Widget], {
 				heading: "original heading",
 				content: "original content",
 				buttonLabel: "original button label",
 				buildRendering: handlebars.compile(
-						"<widgets-in-template>" +
-						"<simple-heading text='{{heading}}'></simple-heading>" +
+					"<handlebars-widgets-in-template>" +
+						"<handlebars-heading text='{{heading}}'></handlebars-heading>" +
 						"<span>{{content}}</span>" +
-						"<button is='test-button' label='{{buttonLabel}}'></button>" +
-						"</widgets-in-template>"
+						"<button is='handlebars-button' label='{{buttonLabel}}'></button>" +
+					"</handlebars-widgets-in-template>"
 				)
 			});
 
 			var myComplexWidget = new ComplexWidget(),
-				headingWidget = myComplexWidget.getElementsByTagName("simple-heading")[0],
+				headingWidget = myComplexWidget.getElementsByTagName("handlebars-heading")[0],
 				buttonWidget = myComplexWidget.getElementsByTagName("button")[0];
 			assert.ok(headingWidget.buildRendering, "heading widget was instantiated");
 			assert.strictEqual(headingWidget.textContent, "original heading",
@@ -192,7 +192,7 @@ define([
 		html: function () {
 			// Testing that parsing still works if ending tags are missing
 
-			var TestHtml = register("test-html", [HTMLUListElement, Widget], {
+			var TestHtml = register("handlebars-html", [HTMLUListElement, Widget], {
 				buildRendering: handlebars.compile("<ul><li>1</li><li><input></ul>")
 			});
 
@@ -217,12 +217,12 @@ define([
 			//		3. tags of SVG nodes are lowercase
 			//		4. attribute names are case sensitive
 
-			var TestSvg = register("test-svg", [HTMLElement, Widget], {
+			var TestSvg = register("handlebars-svg", [HTMLElement, Widget], {
 				buildRendering: svgTmpl
 			});
 
 			var node = new TestSvg();
-			assert.strictEqual(node.tagName.toLowerCase(), "test-svg", "root node exists");
+			assert.strictEqual(node.tagName.toLowerCase(), "handlebars-svg", "root node exists");
 
 			node = node.firstChild;
 			assert.strictEqual(node.tagName, "svg", "svg node exists");
