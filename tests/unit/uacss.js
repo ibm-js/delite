2cd3a9ae88fd33fc641bae0857c244c26705380e
Bill Keese <bill@dojotoolkit.org>
2014-05-02 10:06:39 +0900

    Port dojo/sniff and dojo/uacss to delite since they'll be removed from dojo 2.
    Changes include:
    - make has("ie") return truthy for IE11+ too
    - remove unsupported browsers: blackberry, opera, wii
    - remove quirks mode detection
    - change CSS class names, ex: d-webkit, d-ie-11

---
+++ tests/unit/uacss.js
@@ -0,0 +1,31 @@
+define([
+	"intern!object",
+	"intern/chai!assert",
+	"delite/uacss"
+], function (registerSuite, assert, has) {
+	registerSuite({
+		name: "uacss",
+		uacss: function () {
+			if (has("ie")){
+				assert(/d-ie/.test(document.body.className), "ie");
+				assert(/d-ie-[1-9]/.test(document.body.className), "ie version");
+			}
+			if (has("chrome")) {
+				assert(/d-chrome/.test(document.body.className), "chrome");
+			}
+			if (has("safari")) {
+				assert(/d-safari/.test(document.body.className), "safari");
+			}
+			if (has("ff")) {
+				assert(/d-ff/.test(document.body.className), "firefox");
+			}
+			if (has("ios")) {
+				assert(/d-ios/.test(document.body.className), "ios");
+			}
+			if (has("android")) {
+				assert(/d-android/.test(document.body.className), "android");
+			}
+		}
+	});
+});
+
