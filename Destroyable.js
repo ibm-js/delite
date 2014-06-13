de4c2b123d6794036be1d47248a3d65cf0d54b93
Bill Keese <bill@dojotoolkit.org>
2014-06-13 09:10:07 +0900

    Make Destroyable#own() handle Promises. Also, avoid double destroy when handle has multiple destroy methods (ex: destroy() and remove()). Fixes for dijit bugs #17763, #17680.
    Cherry picked from dijit:
    
    - 797f9a9d5e08aa0e208691e7086a679c8c630e30
    - 0169fa4b37922aaf17cd1ebc034fc00b27353878
    - 9234eeeffcd957152dd4c0307d76aeff76ec933a
    - e5dcf8ee33dadc4a84a4432497dc407d8549f65b

--- Destroyable.js
+++ Destroyable.js
@@ -17,8 +17,12 @@ define([
 		 * @method
 		 */
 		destroy: dcl.advise({
-			before: function () { this._beingDestroyed = true; },
-			after: function () { this._destroyed = true; }
+			before: function () {
+				this._beingDestroyed = true;
+			},
+			after: function () {
+				this._destroyed = true;
+			}
 		}),
 
 		/**
@@ -29,23 +33,52 @@ define([
 		 * @protected
 		 */
 		own: function () {
+			var cleanupMethods = [
+				"destroy",
+				"remove"
+			];
+
 			// transform arguments into an Array
 			var ary = Array.prototype.slice.call(arguments);
 			ary.forEach(function (handle) {
-				var destroyMethodName = "destroy" in handle ? "destroy" : "remove";
-
-				// When this.destroy() is called, destroy handle.  Since I'm using aspect.before(),
-				// the handle will be destroyed before a subclass's destroy() method starts running.
+				// When this.destroy() is called, destroy handle.  Since I'm using advise.before(),
+				// the handle will be destroyed before a subclass's destroy() method starts running, before it calls
+				// this.inherited() or even if it doesn't call this.inherited() at all.  If that's an issue, make an
+				// onDestroy() method and connect to that instead.
+				var destroyMethodName;
 				var odh = advise.before(this, "destroy", function () {
 					handle[destroyMethodName]();
 				});
 
-				// If handle is destroyed manually before this.destroy() is called,
-				// remove the listener set directly above.
-				var hdh = advise.after(handle, destroyMethodName, function () {
+				// Callback for when handle is manually destroyed.
+				var hdhs = [];
+
+				function onManualDestroy() {
 					odh.destroy();
-					hdh.destroy();
-				});
+					hdhs.forEach(function (hdh) {
+						hdh.destroy();
+					});
+				}
+
+				// Setup listeners for manual destroy of handle.
+				// Also computes destroyMethodName, used in listener above.
+				if (handle.then) {
+					// Special path for Promises.  Detect when Promise is resolved, rejected, or
+					// canceled (nb: cancelling a Promise causes it to be rejected).
+					destroyMethodName = "cancel";
+					handle.then(onManualDestroy, onManualDestroy);
+				} else {
+					// Path for other handles.  Just use AOP to detect when handle is manually destroyed.
+					cleanupMethods.forEach(function (cleanupMethod) {
+						if (typeof handle[cleanupMethod] === "function") {
+							if (!destroyMethodName) {
+								// Use first matching method name in above listener.
+								destroyMethodName = cleanupMethod;
+							}
+							hdhs.push(advise.after(handle, cleanupMethod, onManualDestroy));
+						}
+					});
+				}
 			}, this);
 
 			return ary;
