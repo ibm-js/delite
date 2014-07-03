c53fbf64d1060f327e5a2002e204096c5c897e91
Bill Keese <bill@dojotoolkit.org>
2014-07-04 00:16:03 +0900

    Refactor and enhance KeyNav.
    - Efficient implementation of _getNext(), so subclasses don't need to implement it.
    - _getFirst(), _getLast(), _getNavigableChildren() removed.
    - Removed dojo/on and dojo/query dependencies by using this.on(), and manually implementing
       the focusin event delegation.
    
    Migration notes:
    
    - Subclasses should stop defining or _getNext(), _getFirst(), and _getLast(), and should stop referencing
      _getFirst() and _getLast().
      In particular, their implementations of _getNext() may not work anymore; there are new requirements
      that it take a dir parameter, and also that that instance._getNext(instance, 1) gets the first child,
      and instance._getNext(instance, -1) returns the last.
    - Be sure to define childSelector.  It was always required, but now may be even "more necessary"
      than before.

---
+++ tests/unit/KeyNav.js
@@ -0,0 +1,63 @@
+define([
+	"intern!object",
+	"intern/chai!assert",
+	"delite/register",
+	"delite/KeyNav"
+], function (registerSuite, assert, register, KeyNav) {
+	var container;
+
+	var SimpleKeyNav = register("simple-key-nav", [HTMLElement, KeyNav], {
+		childSelector: ".child"
+	});
+
+	registerSuite({
+		name: "KeyNav",
+		setup: function () {
+			container = document.createElement("div");
+			document.body.appendChild(container);
+		},
+
+		_getNext: function () {
+			var skn = new SimpleKeyNav();
+			skn.placeAt(container);
+			skn.innerHTML = "<div label='not a child'>not a child</div>" +
+					"<div>" +
+						"<div class=child label='child 1'></div>" +
+						"<div class=child label='child 2'>" +
+							"<div class=child label='child 3'></div>" +
+							"<div class=child label='child 4'>" +
+								"<div class=child label='child 5'></div>" +
+								"<div label='not a child'>not a child</div> text node " +
+							"</div>" +
+						"</div>" +
+						"<div class=child label='child 6'></div>" +
+					"</div>";
+
+			function scanAll(dir) {
+				var children = [], child = skn;
+				for (var i = 0; i < 7; i++) {
+					child = skn._getNext(child, dir);
+					children.push(child.getAttribute("label"));
+				}
+				return children;
+			}
+			assert.strictEqual(scanAll(1).join(", "), "child 1, child 2, child 3, child 4, child 5, child 6, child 1",
+				"children in natural order");
+
+			assert.strictEqual(scanAll(-1).join(", "), "child 6, child 5, child 4, child 3, child 2, child 1, child 6",
+				"children in reverse order");
+		},
+
+		_getNextOnEmptyContainer: function () {
+			var skn2 = new SimpleKeyNav();
+			skn2.placeAt(container);
+
+			assert.isNull(skn2._getNext(skn2, 1), "forward");
+			assert.isNull(skn2._getNext(skn2, -1), "backward");
+		},
+
+		teardown: function () {
+			container.parentNode.removeChild(container);
+		}
+	});
+});
