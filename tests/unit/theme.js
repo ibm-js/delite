a15294b9209eefc405c3a2612bb178ed2733c2fb
Patrick Ruzand <pruzand@gmail.com>
2014-07-17 08:35:58 +0900

    New css loader plugin, that uses <link> tags to load plain css files. Includes the build capabilities.
    Refs #213, fixes #237.

--- tests/unit/theme.js
+++ tests/unit/theme.js
@@ -2,27 +2,57 @@ define([
 	"require",
 	"intern!object",
 	"intern/chai!assert"
-], function (require, registerSuite, assert) {
+], function (localRequire, registerSuite, assert) {
 
 	function getStyles() {
 		// summary:
 		//		Debugging method to get all the styles in the document.
-		return Array.prototype.map.call(document.getElementsByTagName("style"), function (s) {
-			return "<style>\n" + s.innerHTML.substr(0, 100) + "\n</style>";
+		var sheets = document.styleSheets;
+		return Array.prototype.map.call(sheets, function (s) {
+			var rules = s.cssRules || s.rules;
+			return Array.prototype.map.call(rules, function (r) {
+				return r.cssText;
+			});
 		}).join("\n");
 	}
 
 	registerSuite({
 		name: "theme",
+
 		"load global_css": function () {
 			var d = new this.async(1000);
 			// Load one module that use delite/theme! to load global_css
-			require([
+			localRequire([
 				"./resources/TestThemeWidget1"
 			], d.callback(function () {
 				// global_css should be automatically loaded. It defines class d-reset (themes/common/global.less)
-				assert.strictEqual(getStyles().match(/d-reset/g).length, 1, "global_css is loaded");
+				assert.strictEqual(getStyles().match(/d-reset/g).length, 1, "global is loaded");
 			}));
+			return d;
+		},
+
+		loadLayer: function () {
+			var d = this.async(10000);
+			var layer = "delite/tests/unit/themes/{{theme}}/layer.css";
+
+			require.config({
+				config: {
+					"delite/theme": {
+						layersMap: {
+							"delite/tests/unit/themes/{{theme}}/Button.css": layer,
+							"delite/themes/{{theme}}/global.css": layer,
+						}
+					}
+				}
+			});
+
+			localRequire([
+				"delite/theme!./themes/{{theme}}/Button.css"
+			], d.callback(function () {
+				// layer.css should be loaded instead of Button.css
+				assert.strictEqual(getStyles().match(/deliteBootstrapLayer/g).length, 1, "layer.css inserted once");
+			}));
+
 			return d;
 		}
 	});
