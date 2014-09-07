85c6dc2f9bf59ceab5701ccb98aa07d2a3dd0fb6
Bill Keese <bill@dojotoolkit.org>
2014-09-07 19:32:34 +0900

    popup: stop calling evt.preventDefault() on touchend. It's not needed and actually prevents closing popups (on iOS 7). Fixes #269.

--- popup.js
+++ popup.js
@@ -130,17 +130,6 @@ define([
 
 				widget._popupWrapper = wrapper;
 				advise.after(widget, "destroy", destroyWrapper);
-
-				// Workaround iOS problem where clicking a Menu can focus an <input> (or click a button) behind it.
-				// Need to be careful though that you can still focus <input>'s and click <button>'s in a TooltipDialog.
-				// Also, be careful not to break (native) scrolling of dropdown like ComboBox's options list.
-				if ("ontouchend" in document) {
-					wrapper.addEventListener("touchend", function (evt) {
-						if (!/^(input|button|textarea)$/i.test(evt.target.tagName)) {
-							evt.preventDefault();
-						}
-					});
-				}
 			}
 
 			return wrapper;
