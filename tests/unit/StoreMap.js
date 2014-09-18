e0b23a2a39a8d32443e91040c305e4353c2dea50
Christophe Jolif <cjolif@gmail.com>
2014-09-18 13:29:56 +0200

    StoreMap: fix error on late startup, fixes #283

--- tests/unit/StoreMap.js
+++ tests/unit/StoreMap.js
@@ -48,8 +48,35 @@ define([
 				assert.deepEqual(store.renderItems[1], { id: "fb", foo: "FB", bar: "4" });
 			}));
 			store.startup();
-			var myStore = new M({ data: myData});
+			var myStore = new M({ data: myData });
 			store.store = myStore;
+			return d;
+		},
+
+		// Test case for delite #283.
+		RegularLateStartup: function () {
+			var C = register("test-storemap-late-startup", [HTMLElement, Widget, StoreMap], {
+				fooAttr: "name",
+				barFunc: function (item) {
+					return item.firstname;
+				}
+			});
+			var d = this.async(3000);
+			var store = new C();
+			var myData = [
+				{ id: "foo", name: "Foo", firstname: "1" },
+				{ id: "bar", name: "Bar", firstname: "2" }
+			];
+			var myStore = new M({ data: myData });
+			store.store = myStore;
+			setTimeout(function () {
+				store.startup();
+			}, 1000);
+
+			store.on("query-success", d.callback(function () {
+				// startup() called late, after adding data to the store
+				assert.strictEqual(store.renderItems.length, 2);
+			}));
 			return d;
 		},
 
