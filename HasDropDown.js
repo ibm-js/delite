936e33dafe162919e1c13dbbeb1cbf4c922b856c
Bill Keese <bill@dojotoolkit.org>
2016-06-29 10:11:47 +0900

    HasDropDown: support when KeyNav sets up listeners on sub node. Refs #455.

--- HasDropDown.js
+++ HasDropDown.js
@@ -372,7 +372,19 @@ define([
 			}
 			var dropDown = this._currentDropDown, target = e.target;
 			if (dropDown && this.opened) {
-				if (dropDown.emit("keydown", e) === false) {
+				// Forward the keystroke to the dropdown widget.
+				// Can't use dropDown.emit() though because deliteful/List (the dropdown for deliteful/Combobox)
+				// listens for events on List#containerNode rather than the List root node.
+				var eventCopy = this.ownerDocument.createEvent("HTMLEvents");
+				eventCopy.initEvent("keydown", true, true);
+				for (var i in e) {
+					if (!(i in eventCopy)) {
+						eventCopy[i] = e[i];
+					}
+				}
+
+				var forwardNode = dropDown.keyNavContainerNode || dropDown.containerNode || dropDown;
+				if (forwardNode.dispatchEvent(eventCopy) === false) {
 					/* false return code means that the drop down handled the key */
 					e.stopPropagation();
 					e.preventDefault();
