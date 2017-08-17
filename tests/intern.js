6cbb0e8a0c3dcf5c2f5f1861064e2a1cc80c9dbb
Bill Keese <bill@dojotoolkit.org>
2017-08-17 19:28:24 +0900

    Disable instrumentation to avoid Intern exception on iOS.

--- tests/intern.js
+++ tests/intern.js
@@ -58,8 +58,6 @@ define({
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
 	functionalSuites: [ "delite/tests/functional/all" ],
 
-	// A regular expression matching URLs to files that should not be included in code coverage analysis
-	excludeInstrumentation:
-		/*jshint maxlen:1000*/
-		/^(?:dcl|decor|dijit|dojo|dstore|jquery|lie|requirejs.*|webcomponentsjs|dpointer|delite\/(tests|node_modules)|.*\/themes)\//
+	// Disable code coverage because it gets internal errors on iOS.
+	excludeInstrumentation: true
 });
--- tests/intern.local.js
+++ tests/intern.local.js
@@ -4,13 +4,18 @@ define([
 ], function (intern) {
 	intern.tunnel = "NullTunnel";
 
-
 	// Uncomment this line (and modify machine name) for testing against remote VM.
 	// intern.proxyUrl = "http://mac.local:9000";
 
 	intern.environments = [
 		{ browserName: "chrome" }
 	];
+
+	// Instrumentation is disabled for the remote tests due to internal errors on iOS,
+	// but we can still turn it on locally.
+	intern.excludeInstrumentation =
+		/*jshint maxlen:1000*/
+		/^(?:dcl|decor|dijit|dojo|dstore|jquery|lie|requirejs.*|webcomponentsjs|dpointer|delite\/(tests|node_modules)|.*\/themes)\//;
 
 	return intern;
 });
