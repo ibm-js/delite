97f86b31814e429decfe37e3b2310a2a6fa1f95c
Bill Keese <wkeese@jp.ibm.com>
2021-03-10 10:05:24 +0900

    Ability to set which props do/don't cause a (re)render.
    Note that new Set([...]) is not supported on IE11, so apps
    supporting IE11 without a Set polyfill should create Sets like
    
    var s = new Set();
    [...].forEach(function (prop) { s.add(prop); });

--- LitWidget.js
+++ LitWidget.js
@@ -9,13 +9,52 @@ import Widget from "./Widget";
 import { render as shadyRender } from "lit-html/lib/shady-render";
 
 /**
- * Base class for widgts that render using lit-html.
+ * Base class for widgets that render using lit-html.
  */
 export default dcl([Widget], {
 	/**
 	 * Render using shadow dom.  Cannot be changed dynamically: should be set in a subclass prototype (or here).
 	 */
 	shadow: false,
+
+	/**
+	 * If this is a Set with property names, the widget only (re)renders
+	 * when one of the properties in this list changes.
+	 */
+	propsAffectingRender: null,
+
+	/**
+	 * If this is a Set with property names, the widget doesn't (re)render
+	 * unless a property *not* listed here changes.
+	 */
+	propsNotAffectingRender: null,
+
+	/**
+	 * Return true if the property changes listed in `oldValues` require the widget to rerender.
+	 * @param oldValues
+	 * @returns {boolean}
+	 */
+	needsRender: function (oldValues) {
+		// Reduce rerenders on widget creation by waiting until we are attached to the document for first render.
+		// Then force render the first time we are attached.
+		if (!this.attached) {
+			return false;
+		} else if ("attached" in oldValues) {
+			return true;
+		}
+
+		if (this.propsAffectingRender) {
+			return Object.keys(oldValues).some(function (prop) {
+				return this.propsAffectingRender.has(prop);
+			}.bind(this));
+		} else if (this.propsNotAffectingRender) {
+			return !Object.keys(oldValues).every(function (prop) {
+				return this.propsNotAffectingRender.has(prop);
+			}.bind(this));
+		} else {
+			return true;
+		}
+	},
 
 	/**
 	 * Return widget template filled in with all the widget's current property values.
@@ -32,16 +71,13 @@ export default dcl([Widget], {
 		}
 	},
 
-	refreshRendering: function () {
-		if (!this.attached) {
-			// Reduce rerenders on widget creation by waiting until we are attached to the document for first render.
-			return;
-		}
-
-		if (this.shadow) {
-			shadyRender(this.render(), this.shadowRoot, {scopeName: this.tagName.toLowerCase()});
-		} else {
-			render(this.render(), this);
+	refreshRendering: function (oldValues) {
+		if (this.needsRender(oldValues)) {
+			if (this.shadow) {
+				shadyRender(this.render(), this.shadowRoot, { scopeName: this.tagName.toLowerCase() });
+			} else {
+				render(this.render(), this);
+			}
 		}
 	},
 
